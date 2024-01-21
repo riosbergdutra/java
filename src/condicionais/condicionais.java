0f64473b32422fedacd50931ed4cdf2a281a5a90
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2024-01-21 04:23:32 -0300

    boolean, e e ou

--- src/condicionais/condicionais.java
+++ src/condicionais/condicionais.java
@@ -3,13 +3,13 @@ package condicionais;
 public class condicionais {
     public static void main(String[] args) {
         int idade = 20;
-        int quantidadedePessoas = 2;
-        if(idade >= 18) {
-            System.out.println("ok voce tem mais de 18 anos. "+ "sua idade e " + idade);
-        } else if (quantidadedePessoas >=2){
-            System.out.println("ok, voce pode comprar pois esta acompanhado por um maior de idade");
-        } else {
-            System.out.println("voce nem esta acompanhado, e nem e maior de idade, xispa");
+        int quantidadePessoas = 1;
+        boolean acompanhado = quantidadePessoas >= 2;
+        if(idade >= 18 && acompanhado) {
+            System.out.println("voce pode comprar bebida");
+        }  else {
+            System.out.println("voce nao pode comprar bebida");
         }
+        
     }
 }   
