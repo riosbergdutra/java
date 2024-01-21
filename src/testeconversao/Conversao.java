3d592db5426b96952479eee3d1186a2f868ca359
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2024-01-21 02:25:34 -0300

    variaveis tipos primitivos numeros

--- src/testeconversao/Conversao.java
+++ src/testeconversao/Conversao.java
@@ -6,7 +6,11 @@ public class Conversao {
         int valor = (int) salario;
         System.out.println(valor);
 
+        float pontoFlutuante = (float) 3.14;
+
         int valor2 = 2000000;
         long numeroGrande = 221380938219712789L;
+        short numeroPequeno = 1234;
+        byte b = 127;
     }
 }
