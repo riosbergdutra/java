175e062e755d1da4d417dd9a76d66cc001eeeac3
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2024-01-21 02:37:47 -0300

    string e char '

---
+++ src/caracters/testaCaracters.java
@@ -0,0 +1,10 @@
+package caracters;
+
+public class testaCaracters {
+    public static void main(String[] args) {
+        char letra = 'a';
+        System.out.println(letra);
+        String mensagem = "eu estou aprendendo java";
+        System.out.println(mensagem);
+    }
+}   
---
+++ src/variaveis/TestaVariaveis.java
@@ -0,0 +1,11 @@
+package variaveis;
+
+public class TestaVariaveis {
+    public static void main(String[] args) {
+     int primeiro = 5;
+     int segundo = 7;
+     segundo = primeiro;
+     primeiro = 10;
+     System.out.println(segundo);
+    }
+}
