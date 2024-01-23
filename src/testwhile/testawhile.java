4246160a949335cf0ed0a71209ab4d2a8f0bbae3
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2024-01-23 00:59:13 -0300

    while

---
+++ src/testwhile/testawhile.java
@@ -0,0 +1,11 @@
+package testwhile;
+
+public class testawhile {
+    public static void main(String[] args) {
+        int contador = 0;
+        while (contador <= 10) { 
+            System.out.println(contador);
+            contador ++;
+        }   
+    }
+}
