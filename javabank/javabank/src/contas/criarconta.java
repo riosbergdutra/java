f06fb89cdf8617c39f58d1a6c3b2bd78739bd960
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2024-01-23 02:30:08 -0300

    testando mais conta

--- javabank/javabank/src/contas/criarconta.java
+++ javabank/javabank/src/contas/criarconta.java
@@ -5,5 +5,14 @@ public class criarconta {
         conta primeiraConta = new conta();
         primeiraConta.saldo = 200;
         System.out.println("O saldo da primeira conta e : " + primeiraConta.saldo + " reais.");
+
+        primeiraConta.saldo += 100;
+        System.out.println(primeiraConta.saldo);
+
+        conta segundaConta = new conta();
+        segundaConta.saldo = 50;
+        System.out.println("primeira conta = " + primeiraConta.saldo);
+        System.out.println("segunda conta = " + segundaConta.saldo);
+
     }
 }
